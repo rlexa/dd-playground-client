701e908709bd06561e1738f73a1b0cb465ffd969
rlexa <alexey.rempel@gmail.com>
2018-10-07 22:42:12 +0200

    wip crypto

--- src/app/crypto/crypto-api.service.ts
+++ src/app/crypto/crypto-api.service.ts
@@ -1,10 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { cleanParams, getJsonHeaders } from 'app/util-http';
-import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { map, switchMap, take, tap } from 'rxjs/operators';
+import { WebSocketSubject } from 'rxjs/webSocket';
+
+class BlockchainWebSocketSubject extends WebSocketSubject<Object> {
+  ping = () => this.next({ 'op': 'ping' });
+
+  addressLatestTransaction = (address: string) => {
+    this.next({ 'op': 'addr_sub', 'addr': address });
+    this.next({ 'op': 'ping_tx' });
+    this.next({ 'op': 'addr_unsub', 'addr': address });
+  }
+}
+
+class BlockCypherWebSocketSubject extends WebSocketSubject<Object> {
+  ping = () => this.next({ 'event': 'ping' });
+
+  addressConfirmedTx = (address: string) => this.next({ 'event': 'confirmed-tx', 'address': address });
+}
 
 export interface RequestSkipTake { skip?: number, take?: number }
-
 export interface RequestAddressItems extends RequestSkipTake { address: string }
 
 @Injectable({ providedIn: 'root' })
@@ -12,11 +29,22 @@ export class CryptoApiService {
   constructor(private readonly http: HttpClient, ) { }
 
   private readonly API_BLOCKCHAIN = 'https://blockchain.info';
+  private readonly WS_BLOCKCHAIN = 'wss://ws.blockchain.info/inv';
+  private readonly WS_BLOCKCYPHER = 'wss://socket.blockcypher.com/v1/btc/main';
 
-  bitcoinTransactions$ = (req: RequestAddressItems) => this
+  wsBlockchainPing$ = () => of(this.wsBlockchain()).pipe(tap(_ => _.ping()), switchMap(_ => _), take(1));
+  wsBlockchainAddressLatestTransaction$ = (address: string) => of(this.wsBlockchain()).pipe(tap(_ => _.addressLatestTransaction(address)), switchMap(_ => _), take(1));
+
+  wsBlockCypherPing$ = () => of(this.wsBlockCypher()).pipe(tap(_ => _.ping()), switchMap(_ => _), take(1));
+  wsBlockCypherAddressConfirmedTx$ = (address: string) => of(this.wsBlockCypher()).pipe(tap(_ => _.addressConfirmedTx(address)), switchMap(_ => _), take(1));
+
+  blockchainSingleAddress$ = (req: RequestAddressItems) => this
     .httpGetRest$({ api: this.API_BLOCKCHAIN, url: `/rawaddr/${req.address || 'null'}`, params: { limit: req.take, offset: req.skip, cors: true } })
     .pipe(map(_ => _));
 
   private httpGetRest$ = (val: { api: string, url: string, params?: any }) => this.http
     .get(val.api + val.url, { headers: getJsonHeaders(), params: cleanParams(val.params) });
+
+  private wsBlockchain = () => new BlockchainWebSocketSubject(this.WS_BLOCKCHAIN);
+  private wsBlockCypher = () => new BlockCypherWebSocketSubject(this.WS_BLOCKCYPHER);
 }
--- src/app/widgets/crypto/crypto.component.ts
+++ src/app/widgets/crypto/crypto.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { CryptoApiService } from 'app/crypto';
 import { DoneSubject, rxComplete, rxNext_ } from 'app/rx';
 import { BehaviorSubject, of, Subject } from 'rxjs';
@@ -9,15 +9,14 @@ import { switchMap } from 'rxjs/operators';
   templateUrl: './crypto.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CryptoComponent implements OnDestroy {
+export class CryptoComponent implements OnDestroy, OnInit {
   constructor(
-    private readonly apiCrypto: CryptoApiService,
+    private readonly api: CryptoApiService,
   ) { }
 
   private readonly done$ = new DoneSubject();
 
   readonly bitcoinAddress$ = new BehaviorSubject('1BoatSLRHtKNngkdXEeobR76b53LETtpyT');
-
   readonly anyData$ = new Subject();
 
   ngOnDestroy() {
@@ -25,7 +24,25 @@ export class CryptoComponent implements OnDestroy {
     rxComplete(this.anyData$, this.bitcoinAddress$);
   }
 
-  requestBitcoinAddress = () => of(this.bitcoinAddress$.value || null)
-    .pipe(switchMap(_ => this.apiCrypto.bitcoinTransactions$({ address: _ })))
+  ngOnInit() { }
+
+  apiBlockchainBitcoinAddress = () => of(this.bitcoinAddress$.value || null)
+    .pipe(switchMap(_ => this.api.blockchainSingleAddress$({ address: _ })))
+    .subscribe(rxNext_(this.anyData$), rxNext_(this.anyData$));
+
+  wsBlockchainPing = () => of(null)
+    .pipe(switchMap(_ => this.api.wsBlockchainPing$()))
+    .subscribe(rxNext_(this.anyData$), rxNext_(this.anyData$));
+
+  wsBlockchainAddressLatestTransaction = () => of(this.bitcoinAddress$.value || null)
+    .pipe(switchMap(_ => this.api.wsBlockchainAddressLatestTransaction$(_)))
+    .subscribe(rxNext_(this.anyData$), rxNext_(this.anyData$));
+
+  wsBlockCypherPing = () => of(null)
+    .pipe(switchMap(_ => this.api.wsBlockCypherPing$()))
+    .subscribe(rxNext_(this.anyData$), rxNext_(this.anyData$));
+
+  wsBlockCypherAddressConfirmedTx = () => of(this.bitcoinAddress$.value || null)
+    .pipe(switchMap(_ => this.api.wsBlockCypherAddressConfirmedTx$(_)))
     .subscribe(rxNext_(this.anyData$), rxNext_(this.anyData$));
 }
