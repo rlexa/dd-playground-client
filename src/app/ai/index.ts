30e68d7f8fd0d3cacc10400f43006cacfe9a4f2d
Alex Rempel <alexey.rempel@gmail.com>
2018-12-10 13:07:25 +0100

    tfjs wip

--- src/app/ai/index.ts
+++ src/app/ai/index.ts
@@ -1,6 +1,11 @@
-import { train } from '@tensorflow/tfjs';
+import { ENV, Environment, train } from '@tensorflow/tfjs';
 
-export const OPTIMIZERS = Object.keys(train);
+export const tfjs_OPTIMIZERS = Object.keys(train);
+
+export const tfjs_getBackend = Environment.getBackend;
+export const tfjs_setBackend = Environment.setBackend;
+export const tfjs_getFeatures = ENV.getFeatures;
 
 export * from './ml-polynomial';
 export * from './util';
+
