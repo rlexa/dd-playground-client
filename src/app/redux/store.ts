29c36a6b1ab8f3c92cc80f847195f2bcbdcf6652
Alex Rempel <alexey.rempel@gmail.com>
2018-12-10 13:06:52 +0100

    no redux extension in prod

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,10 +1,11 @@
 import { InjectionToken } from '@angular/core';
-import { Store, StoreEnhancer, compose, createStore } from 'redux';
+import { environment } from 'environments/environment';
+import { compose, createStore, Store, StoreEnhancer } from 'redux';
 import { AppState, redAppState } from './state';
 
 export const AppStore = new InjectionToken('App.store.redux');
 
-const devtools: StoreEnhancer<AppState> = !!window && window['__REDUX_DEVTOOLS_EXTENSION__'] ?
+const devtools: StoreEnhancer<AppState> = !!window && window['__REDUX_DEVTOOLS_EXTENSION__'] && !environment.production ?
   window['__REDUX_DEVTOOLS_EXTENSION__']() : f => f;
 
 export function createAppStore(): Store<AppState> {
