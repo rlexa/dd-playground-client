c140c9f7984666402b287db9baf0e674488c218f
Alex Rempel <alexey.rempel@gmail.com>
2018-12-11 11:50:37 +0100

    ripupper test

--- src/app/module/pipe/ripupper/ripupper.pipe.spec.ts
+++ src/app/module/pipe/ripupper/ripupper.pipe.spec.ts
@@ -1,21 +1,27 @@
 import { RipupperPipe } from './ripupper.pipe';
 
 describe('RipupperPipe', () => {
+  let instance = <RipupperPipe>null;
+
+  beforeEach(() => instance = new RipupperPipe());
+
   test('create an instance', () => {
-    const pipe = new RipupperPipe();
-    expect(pipe).toBeTruthy();
+    expect(instance).toBeTruthy();
   });
 
   test('transform correctly', () => {
-    const pipe = new RipupperPipe();
-    expect(pipe.transform('keynamevalue')).toEqual('keynamevalue');
-    expect(pipe.transform('Keynamevalue')).toEqual('Keynamevalue');
-    expect(pipe.transform('keynameValue')).toEqual('keyname Value');
-    expect(pipe.transform('keyNameValue')).toEqual('key Name Value');
-    expect(pipe.transform('KeyNameValue')).toEqual('Key Name Value');
-    expect(pipe.transform('keynameVAlue')).toEqual('keyname VAlue');
-    expect(pipe.transform('keyname Value')).toEqual('keyname Value');
-    expect(pipe.transform('keyname_Value')).toEqual('keyname_Value');
-    expect(pipe.transform('KEYNAMEVALUE')).toEqual('KEYNAMEVALUE');
+    Object
+      .entries({
+        'keynamevalue': 'keynamevalue',
+        'Keynamevalue': 'Keynamevalue',
+        'keynameValue': 'keyname Value',
+        'keyNameValue': 'key Name Value',
+        'KeyNameValue': 'Key Name Value',
+        'keynameVAlue': 'keyname VAlue',
+        'keyname Value': 'keyname Value',
+        'keyname_Value': 'keyname_Value',
+        'KEYNAMEVALUE': 'KEYNAMEVALUE',
+      })
+      .forEach(([key, val]) => expect(instance.transform(key)).toEqual(val));
   });
 });
