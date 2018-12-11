ae009bae9234c87b9429d8da3a6f906165197b58
Alex Rempel <alexey.rempel@gmail.com>
2018-12-11 11:50:29 +0100

    cfg stuff

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,14 @@ if (environment.production) {
   enableProdMode();
 }
 
+// for change detection profiling
+// platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+//   const applicationRef = ref.injector.get(ApplicationRef);
+//   const appComponent = applicationRef.components[0];
+//   enableDebugTools(appComponent);
+// });
+// in console: ng.profiler.timeChangeDetection();
+// in console: ng.profiler.timeChangeDetection({record:true}); -- will write a dump to Chrome profiler
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
