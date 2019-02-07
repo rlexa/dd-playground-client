5dd2e7e7c7bf17fe5e700205aa21338ce938d2aa
Alex Rempel <alexey.rempel@gmail.com>
2019-02-07 09:28:13 +0100

    hide footer in game-down

--- src/app/module/widget/game-down/game-down-ai-initiative.component.ts
+++ src/app/module/widget/game-down/game-down-ai-initiative.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { resolveInitiative as resolveInitiativeIndices } from 'app/module/widget/game-down/data';
 import { RxStateService, RxStateSetGameDownService } from 'app/rx-state';
+import { RxStateSetUiService } from 'app/rx-state/rx-state-set-ui.service';
 import { trackByIndex } from 'app/util';
 import { DoneSubject, RxCleanup } from 'dd-rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -10,12 +11,14 @@ import { map, shareReplay } from 'rxjs/operators';
   templateUrl: './game-down-ai-initiative.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GameDownAiInitiativeComponent implements OnDestroy {
+export class GameDownAiInitiativeComponent implements OnDestroy, OnInit {
   constructor(
     private readonly rxState: RxStateService,
     private readonly rxStateMutate: RxStateSetGameDownService,
+    private readonly rxStateMutateUi: RxStateSetUiService,
   ) { }
 
+  private wasShowingFooter = false;
   @RxCleanup() private readonly done$ = new DoneSubject();
 
   readonly fields$ = this.rxState.watch(state => state.game.down.scene.fields, this.done$);
@@ -25,7 +28,14 @@ export class GameDownAiInitiativeComponent implements OnDestroy {
 
   trackByIndex = trackByIndex;
 
-  ngOnDestroy() { }
+  ngOnDestroy() {
+    this.rxStateMutateUi.mergeDashboardState({ isVisibleFooter: this.wasShowingFooter });
+  }
+
+  ngOnInit() {
+    this.wasShowingFooter = this.rxState.state.ui.dashboard.isVisibleFooter;
+    this.rxStateMutateUi.mergeDashboardState({ isVisibleFooter: false });
+  }
 
   onClickIndex = (index: number) => this.rxStateMutate.setSceneSelectedIndex(index);
   onHoverIndex = (index: number, hovered: boolean) => this.rxStateMutate.setSceneHoveredIndex(hovered ? index : null);
