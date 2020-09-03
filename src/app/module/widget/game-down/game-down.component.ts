27de02c1bfa13bca5bad00efcd25cd7a9cdfa8c1
rlexa <alexey.rempel@gmail.com>
2020-09-03 22:38:17 +0200

    show di

--- src/app/module/widget/game-down/game-down.component.ts
+++ src/app/module/widget/game-down/game-down.component.ts
@@ -1,7 +1,10 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
 import {watch} from 'dd-rx-state';
+import {Observable, combineLatest} from 'rxjs';
 import {RxStateService, RxStateSetGameDownService} from 'src/app/rx-state';
+import {DiSceneHoveredIndex} from './di-game-down-values';
 import {themeDownDefault, themes} from './theming';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-game-down',
@@ -9,10 +12,16 @@ import {themeDownDefault, themes} from './theming';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GameDownComponent {
-  constructor(private readonly rxState: RxStateService, private readonly rxStateMutate: RxStateSetGameDownService) {
+  constructor(
+    private readonly rxState: RxStateService,
+    private readonly rxStateMutate: RxStateSetGameDownService,
+    @Inject(DiSceneHoveredIndex) public readonly hovered$: Observable<number>,
+  ) {
     this.rxStateMutate.setThemes(themes);
     this.rxStateMutate.setSceneTheme(themes.find((_) => _.name !== themeDownDefault.name).name);
   }
 
-  readonly state$ = this.rxState.state$.pipe(watch((state) => state.game.down.scene));
+  readonly state$ = combineLatest([this.rxState.state$.pipe(watch((state) => state.game.down.scene)), this.hovered$]).pipe(
+    map(([state, hovered]) => ({...state, diHovered: hovered})),
+  );
 }
