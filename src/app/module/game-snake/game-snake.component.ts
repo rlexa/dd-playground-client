b342abd0b3745be8454ca86c559493aba3d2a78d
rlexa <alexey.rempel@gmail.com>
2020-09-04 15:38:47 +0200

    cleanup

--- src/app/module/game-snake/game-snake.component.ts
+++ src/app/module/game-snake/game-snake.component.ts
@@ -1,6 +1,6 @@
 import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
 import {RxCleanup, rxFire_, rxNext_} from 'dd-rxjs';
-import {BehaviorSubject, Subject} from 'rxjs';
+import {BehaviorSubject, merge, Subject} from 'rxjs';
 import {filter, map, withLatestFrom} from 'rxjs/operators';
 import {Game, initGame, onInputDirection, Preset, processFrame, Vector} from './logic-fns';
 
@@ -13,13 +13,36 @@ import {Game, initGame, onInputDirection, Preset, processFrame, Vector} from './
 export class GameSnakeComponent implements OnDestroy, OnInit {
   private timeLast = 0;
 
-  @RxCleanup() public game$ = new BehaviorSubject<Game>(null);
-  @RxCleanup() public preset$ = new BehaviorSubject<Preset>({height: 15, width: 15});
+  @RxCleanup() public readonly game$ = new BehaviorSubject<Game>(null);
+  @RxCleanup() public readonly preset$ = new BehaviorSubject<Preset>({height: 15, width: 15});
 
-  @RxCleanup() public triggerInit$ = new Subject();
-  @RxCleanup() public triggerFrame$ = new Subject();
-  @RxCleanup() public triggerDirection$ = new Subject<string>();
-  @RxCleanup() public toggleLoop$ = new BehaviorSubject(false);
+  @RxCleanup() public readonly triggerInit$ = new Subject();
+  @RxCleanup() public readonly triggerFrame$ = new Subject();
+  @RxCleanup() public readonly triggerDirection$ = new Subject<string>();
+  @RxCleanup() public readonly toggleLoop$ = new BehaviorSubject(false);
+
+  private readonly processDirection$ = this.triggerDirection$.pipe(
+    filter((dir) => ['L', 'U', 'R', 'D'].includes(dir)),
+    map((dir) => ({x: dir === 'L' ? -1 : dir === 'R' ? 1 : 0, y: dir === 'U' ? -1 : dir === 'D' ? 1 : 0} as Vector)),
+    withLatestFrom(this.game$),
+    filter(([vec, game]) => !!game),
+    map(([vec, game]) => onInputDirection(game, vec)),
+  );
+
+  private readonly processFrame$ = this.triggerFrame$.pipe(
+    withLatestFrom(this.game$),
+    map(([_, game]) => game),
+    filter((game) => !!game),
+    map((game) => processFrame(game)),
+  );
+
+  private readonly processInit$ = this.triggerInit$.pipe(
+    withLatestFrom(this.preset$),
+    map(([_, preset]) => preset),
+    map(initGame),
+  );
+
+  private readonly processors$ = merge(this.processDirection$, this.processFrame$, this.processInit$);
 
   triggerInit = rxFire_(this.triggerInit$);
   triggerFrame = rxFire_(this.triggerFrame$);
@@ -33,34 +56,8 @@ export class GameSnakeComponent implements OnDestroy, OnInit {
   }
 
   ngOnInit() {
-    this.triggerFrame$
-      .pipe(
-        withLatestFrom(this.game$),
-        map(([_, game]) => game),
-        filter((game) => !!game),
-        map((game) => processFrame(game)),
-      )
-      .subscribe(rxNext_(this.game$));
-
-    this.triggerInit$
-      .pipe(
-        withLatestFrom(this.preset$),
-        map(([_, preset]) => preset),
-        map(initGame),
-      )
-      .subscribe(rxNext_(this.game$));
-
-    this.triggerDirection$
-      .pipe(
-        filter((dir) => ['L', 'U', 'R', 'D'].includes(dir)),
-        map((dir) => ({x: dir === 'L' ? -1 : dir === 'R' ? 1 : 0, y: dir === 'U' ? -1 : dir === 'D' ? 1 : 0} as Vector)),
-        withLatestFrom(this.game$),
-        filter(([vec, game]) => !!game),
-        map(([vec, game]) => onInputDirection(game, vec)),
-      )
-      .subscribe(rxNext_(this.game$));
-
-    this.toggleLoop$.pipe(filter(Boolean)).subscribe(() => this.onFrame());
+    this.processors$.subscribe(rxNext_(this.game$));
+    this.toggleLoop$.pipe(filter(Boolean)).subscribe(this.onFrame);
   }
 
   private onFrame = (time?: number) => {
