bdad1608f5b437c147663a10a4a19bad3ef570d4
rlexa <alexey.rempel@gmail.com>
2020-10-20 18:58:13 +0200

    wip api

--- src/app/module/widget/route-api/ghibli-movies/ghibli-movie/ghibli-movie.module.ts
+++ src/app/module/widget/route-api/ghibli-movies/ghibli-movie/ghibli-movie.module.ts
@@ -1,11 +1,12 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 import {SimpleViewModule} from '../../../simple-view';
 import {GhibliMovieComponent} from './ghibli-movie.component';
 
 @NgModule({
   declarations: [GhibliMovieComponent],
-  imports: [CommonModule, SimpleViewModule],
+  imports: [CommonModule, MatCardModule, SimpleViewModule],
   exports: [GhibliMovieComponent],
 })
 class GhibliMovieModule {}
