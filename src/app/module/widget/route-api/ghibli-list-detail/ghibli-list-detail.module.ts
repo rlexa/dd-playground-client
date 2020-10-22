cf6f212c468096fc5df78332af62310781874756
rlexa <alexey.rempel@gmail.com>
2020-10-22 12:02:33 +0200

    add sort pipe

--- src/app/module/widget/route-api/ghibli-list-detail/ghibli-list-detail.module.ts
+++ src/app/module/widget/route-api/ghibli-list-detail/ghibli-list-detail.module.ts
@@ -4,10 +4,11 @@ import {NgModule} from '@angular/core';
 import {MatListModule} from '@angular/material/list';
 import {RouterModule} from '@angular/router';
 import {ItemToTitlePipe} from './ghibli-item-title.pipe';
+import {ItemsSortByPipe} from './ghibli-items-sort-by.pipe';
 import {GhibliListDetailComponent} from './ghibli-list-detail.component';
 
 @NgModule({
-  declarations: [GhibliListDetailComponent, ItemToTitlePipe],
+  declarations: [GhibliListDetailComponent, ItemsSortByPipe, ItemToTitlePipe],
   imports: [CommonModule, RouterModule, MatListModule, ScrollingModule],
 })
 class GhibliListDetailModule {}
