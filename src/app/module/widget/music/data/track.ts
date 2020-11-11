630d39392f3a4b441493872a78d76fa23c7d4df4
rlexa <alexey.rempel@gmail.com>
2020-11-11 15:45:00 +0100

    wip chords

--- src/app/module/widget/music/data/track.ts
+++ src/app/module/widget/music/data/track.ts
@@ -1,4 +1,4 @@
-import {fnCompose, fnPadStart, fnSplit} from 'src/app/util/fns';
+import {fnCompose, fnPadEnd, fnSplit} from 'src/app/util/fns';
 
 export interface Meta {
   key: string;
@@ -31,17 +31,62 @@ const trim = fnCompose(trimEnd, trimStart);
 
 const getIndent = (text: string) => (text?.startsWith('\t') ? 1 : 0);
 
-const adjustChords = (text: string) =>
-  text?.replace(/<(.+?)>/g, (match, chord: string, offset: number, inputString: string) => {
-    return `${chord}  `;
+export const TRANSPOSITIONS = 12;
+
+export const normalizeTranspose = (transponse: number) => {
+  transponse = transponse % TRANSPOSITIONS;
+  return transponse >= 0 ? transponse : transponse + TRANSPOSITIONS;
+};
+
+const TONES = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+
+const transposeChord = (transpose: number) => (chord: string) => {
+  transpose = normalizeTranspose(transpose);
+  while (chord.length && transpose > 0) {
+    const iDown = chord.indexOf('b');
+    if (iDown > 0) {
+      chord = chord.substr(0, iDown) + chord.substr(iDown + 1);
+    } else {
+      const iTone = TONES.findIndex((ii) => ii.includes(chord[0]));
+      if (iTone >= 0) {
+        const iUp = chord.indexOf('#');
+        if (iUp > 0) {
+          chord = chord.substr(0, iUp) + chord.substr(iUp + 1);
+          chord = TONES[(iTone + 1) % TONES.length] + chord.substr(1);
+        } else {
+          const tone = TONES[iTone];
+          if (tone === 'E' || tone === 'B') {
+            chord = TONES[(iTone + 1) % TONES.length] + chord.substr(1);
+          } else if (tone === 'A') {
+            chord = 'Bb' + chord.substr(1);
+          } else {
+            chord += '#';
+          }
+        }
+      } else {
+        chord = `${chord}#`;
+      }
+    }
+    --transpose;
+  }
+  return chord;
+};
+
+const normalizeChord = (chord: string) => chord?.replace(/H/g, 'B');
+
+const adjustChords = (transpose: number) => (text: string) =>
+  normalizeChord(text)?.replace(/<(.+?)>/g, (match, chord: string, offset: number, inputString: string) => {
+    const chordNew = transposeChord(transpose)(chord);
+    return fnPadEnd(' ')(chord.length + 2)(chordNew);
   });
 
-const normalizeTextLine = (text: string) =>
-  adjustChords(text)
+const normalizeTextLine = (transpose: number) => (text: string) =>
+  adjustChords(transpose)(text)
     ?.replace(/\t/g, '')
     .replace(/(<[^(><.)]+>)/g, '')
     .replace(/<>/g, '');
 
-const textToLine = (text: string): Line => ({indent: getIndent(text), text: normalizeTextLine(text)});
+const textToLine = (transpose: number) => (text: string): Line => ({indent: getIndent(text), text: normalizeTextLine(transpose)(text)});
 
-export const textToLines = (text: string) => splitBreaksWin(trim(text)).flatMap(splitBreaksMac).flatMap(splitBreaksUnix).map(textToLine);
+export const textToLines = (text: string, transpose: number) =>
+  splitBreaksWin(trim(text)).flatMap(splitBreaksMac).flatMap(splitBreaksUnix).map(textToLine(transpose));
--- src/app/module/widget/music/track/track-text/track-text.component.ts
+++ src/app/module/widget/music/track/track-text/track-text.component.ts
@@ -1,8 +1,9 @@
 import {ChangeDetectionStrategy, Component, Input, OnDestroy} from '@angular/core';
 import {RxCleanup, StateSubject} from 'dd-rxjs';
+import {combineLatest} from 'rxjs';
 import {map} from 'rxjs/operators';
 import {trackByIndex} from 'src/app/util';
-import {textToLines} from '../../data/track';
+import {normalizeTranspose, textToLines} from '../../data/track';
 
 @Component({
   selector: 'app-track-text',
@@ -12,6 +13,7 @@ import {textToLines} from '../../data/track';
 })
 export class TrackTextComponent implements OnDestroy {
   @RxCleanup() private readonly data$ = new StateSubject<string>(null);
+  @RxCleanup() public readonly transpose$ = new StateSubject(0);
 
   readonly trackByIndex = trackByIndex;
 
@@ -19,7 +21,15 @@ export class TrackTextComponent implements OnDestroy {
     this.data$.next(val || null);
   }
 
-  public readonly lines$ = this.data$.pipe(map(textToLines));
+  @Input() set transpose(val: number) {
+    this.transpose$.next(normalizeTranspose(val ?? 0));
+  }
+
+  public readonly lines$ = combineLatest([this.data$, this.transpose$]).pipe(map(([data, transpose]) => textToLines(data, transpose)));
+
+  addTranspose = (add: number) => {
+    this.transpose = this.transpose$.value + add;
+  };
 
   destroy() {}
   ngOnDestroy() {
