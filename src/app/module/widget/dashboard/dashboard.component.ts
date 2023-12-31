23fa4a99f41a79bd7987d1cd1a2da2166e00d956
Alex Rempel <alexey.rempel@googlemail.com>
2023-12-31 11:46:22 +0100

    fix routes

--- src/app/module/widget/dashboard/dashboard.component.ts
+++ src/app/module/widget/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import {CommonModule} from '@angular/common';
 import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {FooterComponent} from '../footer';
-import {NavigationBarComponent} from '../navigation-bar';
+import {NavigationBarComponent, NavigationBarItemsFromRouteDirective} from '../navigation-bar';
 import {DiDashboardVisibilityFooter} from './di-dashboard-options';
 
 @Component({
@@ -11,7 +11,7 @@ import {DiDashboardVisibilityFooter} from './di-dashboard-options';
   styleUrls: ['./dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, FooterComponent, NavigationBarComponent, RouterModule],
+  imports: [CommonModule, FooterComponent, NavigationBarComponent, NavigationBarItemsFromRouteDirective, RouterModule],
 })
 export class DashboardComponent {
   readonly isVisibleFooter$ = inject(DiDashboardVisibilityFooter);
